364ef4ff03cca6177919cb0e2680c31bb12196e0
tchoutdara <monkeymilitia444@gmail.com>
2018-09-04 10:27:47 -0600

    generate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "subs#index"
+
+  resources :subs
+
+  # resources :subs, only: [:new, :create, :edit, :update]
+  # resources :subs, except: [:index, :show, :destroy]
+
+  # get "/subs", to: "subs#index"
+  # get "/subs/:id", to: "subs#show"
+  # delete "/subs/:id", to: "subs#destroy"
+
 end
