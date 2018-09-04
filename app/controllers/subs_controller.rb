a00e12d50e1d1b25a63d5b7394e77071da3ce062
tchoutdara <monkeymilitia444@gmail.com>
2018-09-04 12:09:17 -0600

    make another model

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,25 +1,53 @@
 class SubsController < ApplicationController
+
+before_action :set_sub, only: [:show, :edit, :update, :destroy]
+
   def index
     @subs = Sub.all
   end
 
   def show
-    @sub = Sub.find(params[:id])
   end
 
   def new
+    @sub = Sub.new
+    render partial: "form"
   end
 
   def create
+    @sub = Sub.new(sub_params)
+    
+    if @sub.save
+      redirect_to subs_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+
+    if @sub.update(sub_params)
+      redirect_to sub_path(@sub.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @sub.destroy
+    redirect_to subs_path
   end
+
+  private
+    def set_sub
+      @sub = Sub.find(params[:id])
+    end
+
+    def sub_params
+      params.require(:sub).permit(:name)
+    end
 
 end
